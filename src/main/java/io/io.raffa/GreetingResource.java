87d58c80a28e6327db3e197fe5934d1fdcd35d2e
raffa <raffa@che>
2023-03-12 18:26:27 +0000

    ciao

--- src/main/java/io/io.raffa/GreetingResource.java
+++ src/main/java/io/io.raffa/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy ciao";
+        return "Hello RESTEasy ciao ciao";
     }
 }
